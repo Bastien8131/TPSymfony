e3042a24d479f1a271d7d232eae7f3e511ec044d
Ast415 <bastienbories81@gmail.com>
2023-03-10 14:18:34 +0100

    first push

---
+++ src/Controller/TestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TestController extends AbstractController {
+    public function nbaleatoire() {
+        $nb = random_int(0, 100);
+        return $this->render('nbaleatoire.html.twig', [
+        'lenombresorti' => $nb
+        ]);
+    }
+    
+    public function dateMaintenant(){
+        $date = getdate();
+        return $this->render('maintenant.html.twig', [
+        'dateMaintenant' => $date
+        ]);
+    }
+}
+
+?>
+
+<?php /*
+
+namespace App\Controller ;
+
+use Symfony\Component\HttpFoundation\Response ;
+
+class TestController{
+    public function nbaleatoire(){
+        $nb = random_int ( 0 , 100 );
+        return new Response (
+        '<html><body>Résultat : ' . $nb . '</body></html>'
+        );
+    }
+}*/
+
+?>
